d5bed2b9fc575158e4abe0e075931d23ca5d5668
Adam Januszewski <Adpjan@att.net>
2022-05-09 16:20:50 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by adam janusewski on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
